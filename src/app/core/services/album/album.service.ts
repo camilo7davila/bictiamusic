e6e86dee0c98d579086daeed5ab335dd204ab320
Damamapi <ddamamapi@gmail.com>
2020-04-10 14:39:28 -0500

    finished album service

--- src/app/core/services/album/album.service.ts
+++ src/app/core/services/album/album.service.ts
@@ -5,14 +5,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AlbumService {
+  private url = 'https://bictiamusic.herokuapp.com/album';
+  private authorId = localStorage.getItem('id');
 
   constructor(
     private http: HttpClient
   ) { }
 
-  private url = 'https://bictiamusic.herokuapp.com'
 
   getAlbums() {
-    return this.http.get<any>(this.url+'/album')
+    return this.http.get<any>(this.url);
+  }
+
+  getAlbumByAuthor() {
+    return this.http.get<any>(this.url + '/listbyauthor/' + this.authorId);
+  }
+
+  postAlbum(album: any) {
+    return this.http.post(this.url, album);
   }
 }
