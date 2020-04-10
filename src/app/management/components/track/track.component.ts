c7f5d5e7fa1a77ca1a4d51ac91d16dc830c6ebd0
Camilo Davila <camilo7davila@gmail.com>
2020-04-10 12:41:10 -0500

    no sirve subir file

--- src/app/management/components/track/track.component.ts
+++ src/app/management/components/track/track.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SongService } from 'src/app/core/services/song/song.service';
 
- 
+
 
 @Component({
   selector: 'app-track',
@@ -20,35 +20,27 @@ export class TrackComponent implements OnInit {
     this.buildForm();
   }
   img$: Observable<any>;
-  input: string = ''
-  
+  img2$: Observable<any>
+
   ngOnInit(): void {
   }
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      nameSong: [''],
-      idGener: [''],
-      idAuthor: [''],
-      idAlbum: [''],
-      qualification: [''],
-      durationSong: ['']
+      nameSong: ['', [Validators.required]],
+      idGener: ['', [Validators.required]],
+      idAuthor: ['', [Validators.required]],
+      idAlbum: ['', [Validators.required]],
+      qualification: ['', [Validators.required]],
+      imgSong: [''],
+      songFile: ['']
     })
   }
 
-  save() {
-    let songData = this.form.value;
-    this.songService.postSong(songData)
-      .subscribe(data => {
-        console.log(data);
-        songData === data;
-      }, e => console.log(e))
-    console.log(this.form.value)
-  }
-  
-  uploadFile(event) {
+
+  uploadImage(event) {
     const file = event.target.files[0];
-    const dir = `imagenprueba/${file.name}`;
+    const dir = `/${this.form.get('idAuthor').value}/${file.name}`;
     const fileRef = this.storage.ref(dir);
     const task = this.storage.upload(dir, file);
     alert('se subio la foto')
@@ -57,11 +49,37 @@ export class TrackComponent implements OnInit {
       finalize(async () => {
         this.img$ = await fileRef.getDownloadURL();
         this.img$.subscribe((url) => {
-          console.log(url);
-          this.input = url
+          this.form.get('imgSong').setValue(url)
         })
       })
     ).subscribe();
   }
 
+  uploadMP3(event){
+    const file = event.target.files[0];
+    const dir = `/${this.form.get('idAuthor').value}/${file.name}`;
+    const fileRef = this.storage.ref(dir);
+    const task = this.storage.upload(dir, file);
+    alert('se subio la foto')
+
+    task.snapshotChanges().pipe(
+      finalize(async () => {
+        this.img2$ = await fileRef.getDownloadURL();
+        this.img2$.subscribe((url) => {
+          this.form.get('songFile').setValue(url)
+        })
+      })
+    ).subscribe();
+  }
+
+  save() {
+    console.log(this.form.value);
+    // let songData = this.form.value;
+    // this.songService.postSong(songData)
+    //   .subscribe(data => {
+    //     console.log(data);
+    //     songData === data;
+    //   }, e => console.log(e))
+    // console.log(this.form.value)
+  }
 }
