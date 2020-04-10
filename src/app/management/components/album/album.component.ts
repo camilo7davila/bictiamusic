b8dd7190434acb8483d153c2a9aa22c77ffd872a
Damamapi <ddamamapi@gmail.com>
2020-04-10 14:47:05 -0500

    album component upload

--- src/app/management/components/album/album.component.ts
+++ src/app/management/components/album/album.component.ts
@@ -11,6 +11,7 @@ import { AlbumService } from 'src/app/core/services/album/album.service';
 export class AlbumComponent implements OnInit {
   
   form: FormGroup;
+  img$: Observable<any>;
 
   constructor(
     private fb: FormBuilder,
@@ -28,6 +29,33 @@ export class AlbumComponent implements OnInit {
       dateAlbum: [''],
       idAuthor: ['']
     })
+  }
+
+  uploadImage(event) {
+    const file = event.target.files[0];
+    const dir = `/${this.form.get('idAuthor').value}/${file.name}`;
+    const fileRef = this.storage.ref(dir);
+    const task = this.storage.upload(dir, file);
+    alert('se subio la foto')
+
+    task.snapshotChanges().pipe(
+      finalize(async () => {
+        this.img$ = await fileRef.getDownloadURL();
+        this.img$.subscribe((url) => {
+          this.form.controls['imgSong'].setValue(url)
+        })
+      })
+    ).subscribe();
+  }
+
+  save() {
+    console.log(this.form.value)
+    // let albumData = this.form.value;
+    // this.albumService.postAlbum(albumData)
+    //   .subscribe(data => {
+    //     console.log(data);
+    //     albumData === data;
+    //   }, e => console.log(e))
   }
 
 }
